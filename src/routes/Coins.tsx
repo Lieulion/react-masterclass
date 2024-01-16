a5201935f71703b92003be10945b92d9c68161c9
[Lieulion] <[l59565900@gmail.com]>
2024-01-16 19:31:35 +0900

    commit

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  //useQuery hook은 FetcherFunction을 부르고, fetcher함수가loading이라면 알려줌
+  //React Query가 fetcher함수를 끝내면 data에 json을 넣을것.
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins); //Query key 및 Fetcher Fucntion 필요
+  /* react Query를 위해 주석처리
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      // React Query를 사용하기 위해 api.ts로 옮김
+      // const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      // const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []); */
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png
+                  `}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
+}
+export default Coins;
